c0987fa8127846e03c150d8ac3ea44d3453a58a8
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2019-12-10 21:14:57 +0000

    Alignment issue addressed

--- src/components/ShoppingItem.vue
+++ src/components/ShoppingItem.vue
@@ -1,30 +1,38 @@
 <template>
   <article class="card shopping-item">
     <header class="shopping-item-header">
-      <img :src="item.image" :alt="item.name">
+      <img :src="item.image" :alt="item.name" />
       <div class="shopping-item-title">{{item.name}}</div>
     </header>
     <footer class="shopping-item-footer">
       <span class="button-group">
-        <button class="btn-item btn-unit" @click="onDecrementClick"><RemoveIcon class="icon-small"/></button>
+        <button class="btn-item btn-unit" @click="onDecrementClick">
+          <RemoveIcon class="icon-small" />
+        </button>
         <span class="unit-title">{{activeItem.unit.value}} {{item.unit.name}}</span>
-        <button class="btn-item btn-unit" @click="onIncrementClick"><AddIcon class="icon-small" /></button>
+        <button class="btn-item btn-unit" @click="onIncrementClick">
+          <AddIcon class="icon-small" />
+        </button>
       </span>
-      <p><button class="btn-item btn-chart" @click="onCartClick"><CartIcon class="icon-small"/></button></p>
+      <p>
+        <button class="btn-item btn-chart" @click="onCartClick">
+          <CartIcon class="icon-small" />
+        </button>
+      </p>
     </footer>
   </article>
 </template>
 
 <script>
-import CartIcon from '@/icons/CartIcon'
-import AddIcon from '@/icons/AddIcon'
-import RemoveIcon from '@/icons/RemoveIcon'
+import CartIcon from "@/icons/CartIcon";
+import AddIcon from "@/icons/AddIcon";
+import RemoveIcon from "@/icons/RemoveIcon";
 export default {
   name: "ShoppingItem",
   data() {
     return {
       activeItem: JSON.parse(JSON.stringify(this.item))
-    }
+    };
   },
   props: {
     item: Object
@@ -36,16 +44,18 @@ export default {
   },
   methods: {
     onIncrementClick() {
-      this.activeItem.unit.value = this.activeItem.unit.value + this.activeItem.unit.increment
+      this.activeItem.unit.value =
+        this.activeItem.unit.value + this.activeItem.unit.increment;
     },
     onDecrementClick() {
       if (this.activeItem.unit.value > this.activeItem.unit.increment) {
-        this.activeItem.unit.value = this.activeItem.unit.value - this.activeItem.unit.increment
+        this.activeItem.unit.value =
+          this.activeItem.unit.value - this.activeItem.unit.increment;
       }
     },
     onCartClick() {
-      this.$store.commit('ADD_TO_CART', this.activeItem)
-      this.activeItem = this.item
+      this.$store.commit("ADD_TO_CART", this.activeItem);
+      this.activeItem = this.item;
     }
   }
 };
@@ -56,9 +66,9 @@ export default {
   flex: 0 1 49.5%;
 }
 .shopping-item {
-  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
-  min-width: 150px; 
+  min-width: 150px;
   max-width: 220px;
   padding: 10px;
   padding-bottom: 8px;
@@ -79,26 +89,28 @@ export default {
   display: inline-block;
   font-size: small;
   border: 1px solid #ddd;
-  line-height: 36px;
-  padding:0;
+  line-height: 34px;
+  max-height: 36px;
+  padding: 0;
 }
 .btn-item {
   border: none;
   border-top: 1px solid #ddd;
   border-bottom: 1px solid #ddd;
   line-height: 36px;
+  max-height: 36px;
   font-size: small;
   cursor: pointer;
   outline: none;
 }
 .btn-item:hover {
-	background-color: tomato;
-	fill: #000;
+  background-color: tomato;
+  fill: #000;
 }
 .btn-item:active {
-	transform: translateY(2px);
-	fill: #fff;
-	color: #fff;
+  transform: translateY(2px);
+  fill: #fff;
+  color: #fff;
 }
 .btn-unit {
   padding: 0 10px;
@@ -121,10 +133,10 @@ export default {
   background-color: tomato;
 }
 .btn-chart:hover {
-	background-color: tomato;
-	fill: #000;
+  background-color: tomato;
+  fill: #000;
 }
 .btn-chart:active {
   fill: #fff;
-}  
+}
 </style>
