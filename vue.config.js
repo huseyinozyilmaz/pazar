02b201415710e1f86e7e810fb392e6155905beaa
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2019-12-10 21:15:37 +0000

    Latest dependency updates

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+  outputDir: path.join(__dirname, 'server', 'public'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081'
+      }
+    }
+  },
+  configureWebpack: {
+    devtool: 'source-map'
+  }
+}
