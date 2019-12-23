e52c480b2233f8df894c0dd2a68da60649965dd3
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2019-12-23 18:48:47 +0000

    Limit the body payload to prevent DOS attacks

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const mail = require('./mail')
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '10kb' }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/api/shop/section', (req, res) => {
