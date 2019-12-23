2ea83d78178bba9d9f1dff9913682f391c41ac28
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2019-12-23 18:53:04 +0000

    Preventing DOS Attacks with api limiter

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const rateLimit = require("express-rate-limit")
 const port = process.env.PORT || 8081
 const shop = require('./shop')
 const profiles = require('./profiles')
 const mail = require('./mail')
 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+})
+
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '10kb' }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use("/api/", apiLimiter)
 
 app.get('/api/shop/section', (req, res) => {
   res.json(shop.getSections())
